7202f6d8c207e3b3605bf3aafd4e9013e8b20a41
Jiaqi Ji <iamjijiaqi@gmail.com>
2026-06-14 08:07:11 +0800

    fix: add question length limit to prevent oversized LLM input

--- src/main/java/com/knowhub/dto/ChatRequest.java
+++ src/main/java/com/knowhub/dto/ChatRequest.java
@@ -2,6 +2,7 @@ package com.knowhub.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -11,6 +12,7 @@ public class ChatRequest {
     private Long knowledgeBaseId;
 
     @NotBlank
+    @Size(max = 2000, message = "问题长度不能超过2000字")
     private String question;
 
     private Long conversationId;
